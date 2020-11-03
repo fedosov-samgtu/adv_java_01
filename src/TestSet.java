009ee451f93069c6ce9ec9d02208ac3df35ab95f
fedosov.samgtu <fedosov.samgtu@gmail.com>
2020-11-03 22:36:48 +0400

    add TestSet.java

---
+++ src/TestSet.java
@@ -0,0 +1,90 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TestSet {
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Set<String> treeSet = new TreeSet<>();
+
+        hashSet.add("Mike");
+        hashSet.add("Katy");
+        hashSet.add("Tom");
+        hashSet.add("George");
+        hashSet.add("Donald");
+        hashSet.add("Tom");
+        hashSet.add("Tom");
+
+        for (String name : hashSet) {
+            System.out.println(name);
+        }
+
+
+        System.out.println();
+        linkedHashSet.add("Mike");
+        linkedHashSet.add("Katy");
+        linkedHashSet.add("Tom");
+        linkedHashSet.add("George");
+        linkedHashSet.add("Donald");
+
+        for (String name : linkedHashSet) {
+            System.out.println(name);
+        }
+
+        System.out.println();
+        treeSet.add("Mike");
+        treeSet.add("Katy");
+        treeSet.add("Tom");
+        treeSet.add("George");
+        treeSet.add("Donald");
+
+        for (String name : treeSet) {
+            System.out.println(name);
+        }
+
+
+        System.out.println();
+        System.out.println(hashSet.contains("Tom"));
+        System.out.println(hashSet.contains("Tim"));
+
+        System.out.println(hashSet.isEmpty());
+
+        System.out.println(hashSet);
+
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(0);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+
+        // union объединение множеств
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println(union);
+
+        // intersection пересечение множеств
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println(intersection);
+
+        // difference разность множеств
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        System.out.println(difference);
+
+
+    }
+}
